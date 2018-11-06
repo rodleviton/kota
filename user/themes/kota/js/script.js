33e8e6146e87b338e4aa150417ea8684461c719e
Rod Leviton <rod@rodleviton.com>
2018-11-06 12:24:01 +1100

    add sections

--- user/themes/kota/js/script.js
+++ user/themes/kota/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Smooth scroll
   smoothScroll.init({
     selector: ".nav-link", // Selector for links (must be a valid CSS selector)
     selectorHeader: "#navbar", // Selector for fixed headers (must be a valid CSS selector)
@@ -7,4 +8,21 @@ $(document).ready(function() {
     offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
     updateURL: false // Boolean. If true, update the URL hash on scroll
   });
+
+  // Testimonial carousel
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      }
+    }
+  });
+
+  // FAQs Accordion
+  $(".accordion").collapse();
 });
